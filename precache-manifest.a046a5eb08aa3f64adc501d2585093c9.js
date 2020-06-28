bb662547cf0629ad823a9e9a0bfab1d71074ff02
Cristian <triscu.cristian@gmail.com>
2020-06-28 14:11:43 +0300

    Updates

---
+++ precache-manifest.a046a5eb08aa3f64adc501d2585093c9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "96729b7bfc17edddbde0a37b58eeff71",
+    "url": "/test/index.html"
+  },
+  {
+    "revision": "ac8669dc12328af6fda2",
+    "url": "/test/static/css/main.462d3aaf.chunk.css"
+  },
+  {
+    "revision": "98d46ca4b85c3eb2b3f6",
+    "url": "/test/static/js/2.7a1bccef.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/test/static/js/2.7a1bccef.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ac8669dc12328af6fda2",
+    "url": "/test/static/js/main.6715138d.chunk.js"
+  },
+  {
+    "revision": "e621e811fda85ce6bcdb",
+    "url": "/test/static/js/runtime-main.453d4c6d.js"
+  }
+]);
